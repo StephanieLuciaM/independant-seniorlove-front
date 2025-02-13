6b424c2097a28fb74259ac47a199c6fe4671ee9a
Elia Berthier <eliaberthier@MacBook-Air-de-Elia.local>
2025-02-13 17:24:40 +0100

    put the templates of questions in index.html

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,10 +1,8 @@
 import { fetchDisplayHomePageVisitor} from "./homepage.visitor.js";
-import { displayOneSlide } from "./signup.js";
 
 init();
 
 async function init(){
   await fetchDisplayHomePageVisitor();
-  displayOneSlide();
 }
 
