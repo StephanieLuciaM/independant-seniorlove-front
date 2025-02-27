808d461b2b56212270b6048b660a6938a48bea1d
Elia Berthier <eliaberthier@MacBook-Air-de-Elia.local>
2025-02-27 09:19:07 +0100

    change event url

--- src/assets/js/homepage.connected.js
+++ src/assets/js/homepage.connected.js
@@ -71,7 +71,7 @@ function addEventsButtonListener(data){
     // Fetch and display the "Évènements" page with the provided data
     fetchDisplayEventsPage(data);
     const state = {page: "Évènements", initFunction: 'fetchDisplayEventsPage'};
-    const url = "/évènements";
+    const url = "/evenements";
     history.pushState(state, "", url);
   });
 };
